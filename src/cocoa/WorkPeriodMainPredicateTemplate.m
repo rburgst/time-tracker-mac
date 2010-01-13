aac4d87e95fd498d1ae5f55220ffd8ce27543d8a
rburgst <rburgst@localhost>
2010-01-13 20:09:47 +0000

    removed useless code

--- src/cocoa/WorkPeriodMainPredicateTemplate.m
+++ src/cocoa/WorkPeriodMainPredicateTemplate.m
@@ -20,35 +20,6 @@
     return 0.0;
 }
 
-- (NSArray*) templateViews {
-    if (_templateViews != nil) {
-        return _templateViews;
-    }
-    NSMutableArray *result = [NSMutableArray arrayWithArray:[super templateViews]];
-    //NSPopUpButton *search = [result objectAtIndex:0];
-//    [search selectItemAtIndex:0];
-    
-    NSButton *applyBtn = [[NSButton alloc] initWithFrame:NSMakeRect(0.0, 0.0, 100.0, 18.0)];
-    applyBtn.title = @"Apply";
-    [applyBtn setBezelStyle:NSRoundRectBezelStyle];
-  //  applyBtn.font = search.font;
-    [applyBtn.cell setControlSize:NSMiniControlSize];
-    [applyBtn sizeToFit];
-    [result addObject:applyBtn];
-    [applyBtn release];
-    
-    NSButton *saveBtn = [[NSButton alloc] initWithFrame:NSMakeRect(0.0, 0.0, 100.0, 18.0)];
-    saveBtn.title = @"Save";
-    saveBtn.bezelStyle = NSRoundRectBezelStyle;
-//    saveBtn.font = search.font;
-    [saveBtn.cell setControlSize:NSMiniControlSize];
-    [saveBtn sizeToFit];
-    [result addObject:saveBtn];
-    [saveBtn release];
-    self._templateViews = result;
-    return result;
-}
-
 -(void) dealloc {
     self._templateViews = nil;
     [super dealloc];
