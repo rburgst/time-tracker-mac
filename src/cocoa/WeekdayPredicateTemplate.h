ae6d3d559983b250f1fc9bc223e795e39fc254bc
rburgst <rburgst@localhost>
2010-03-09 20:26:36 +0000

    new predicatetemplate for weekday filter

---
+++ src/cocoa/WeekdayPredicateTemplate.h
@@ -0,0 +1,14 @@
+//
+//  WeekdayPredicateTemplate.h
+//
+//  Created by Rainer Burgstaller on 27.02.10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface WeekdayPredicateTemplate : NSPredicateEditorRowTemplate {
+
+}
+
+@end
---
+++ src/cocoa/WeekdayPredicateTemplate.m
@@ -0,0 +1,42 @@
+//
+//  WeekdayPredicateTemplate.m
+//
+//  Created by Rainer Burgstaller on 27.02.10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "WeekdayPredicateTemplate.h"
+
+@implementation WeekdayPredicateTemplate
+- (double)matchForPredicate:(NSPredicate *)predicate {
+    if (![predicate isKindOfClass:[NSComparisonPredicate class]]) {
+        return 0.0;
+    }
+    NSComparisonPredicate *compPred = (NSComparisonPredicate*)predicate;
+    NSExpression *leftExp = [compPred leftExpression];
+    if (leftExp.expressionType == NSKeyPathExpressionType) {
+        if ([leftExp.keyPath isEqualToString:@"weekday"]) {
+            return 1.0;
+        }
+    }
+    double result = [super matchForPredicate:predicate];
+    return result;
+}    
+
+- (NSPredicate *)predicateWithSubpredicates:(NSArray *)subpredicates
+{
+    NSPopUpButton *popup = (NSPopUpButton*) [[self templateViews] objectAtIndex:2];
+    return [NSComparisonPredicate predicateWithFormat:@"weekday == %d", [popup selectedTag]];
+}    
+
+
+- (void)setPredicate:(NSPredicate *)predicate {
+    NSComparisonPredicate *compPred = (NSComparisonPredicate*)predicate;
+    NSArray *views = [self templateViews];
+    NSPopUpButton *last = [views objectAtIndex:2];
+    NSExpression *rightExp = [compPred rightExpression];
+    NSNumber *num = [rightExp constantValue];
+    [last selectItemAtIndex:num.intValue];
+}
+
+@end
